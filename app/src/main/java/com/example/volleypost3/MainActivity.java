1a57eb0dae6a429e911d5b0dbaeeeedb4a33f43b
sachitha2 <shpsachitha@gmail.com>
2019-12-10 21:37:45 +0530

    Init

---
+++ app/src/main/java/com/example/volleypost3/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.volleypost3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+        js();
+    }
+
+
+    void js(){
+      String  url = "http://192.168.1.100/shop/APP/TEST.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error+"");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+                params.put("data", "chatson");
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+}
